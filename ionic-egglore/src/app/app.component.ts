7afcde153a5ef8528609f438131eb60fa186c6d3
martin <martin@esls.io>
2018-03-10 18:32:01 +0900

    jhun

---
+++ ionic-egglore/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = TabsPage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translate: TranslateService) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+
+      translate.addLangs(["en_US", "ko_KR"]);
+    
+      translate.setDefaultLang('en_US');
+
+      console.log('>>>>>'+translate.getBrowserLang());
+      translate.use('ko_KR');
+    });
+  }
+}
